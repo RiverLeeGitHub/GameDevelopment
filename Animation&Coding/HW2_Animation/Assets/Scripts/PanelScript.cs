221a8453d6e00bccb32aa284a0b187fdb83a086f
Jiang Li <lijiang_1998@hotmail.com>
2020-02-12 01:20:30 -0500

    commit

---
+++ Animation&Coding/HW2_Animation/Assets/Scripts/PanelScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PanelScript : MonoBehaviour
+{
+    private void Start()
+    {
+        if (SceneManager.GetSceneByName("Scene2").isLoaded)
+        {
+            SceneManager.UnloadSceneAsync("Scene2");
+        }
+        if (SceneManager.GetSceneByName("Scene1").isLoaded)
+        {
+            SceneManager.UnloadSceneAsync("Scene1");
+        }
+        //SceneManager.UnloadSceneAsync("Scene1");
+        //SceneManager.UnloadSceneAsync("Scene2");
+    }
+
+    public void onClick1()
+    {
+        Debug.Log("onClick1");
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void onClick2()
+    {
+        Debug.Log("onClick2");
+        SceneManager.LoadScene("Scene2");
+    }
+
+}
---
+++ SceneConstruction/HW1/Assets/Scripts/README.txt
@@ -0,0 +1,7 @@
+Dear grader,
+
+Since I forgot to save the video recorded version of scene, I kept that recorded video but rewrote the scene (made it with required functionality again), so there is sightly difference between the video and the current scene. 
+
+Thanks for your understanding!
+
+Jiang
